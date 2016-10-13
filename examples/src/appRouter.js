804c00082efaba619e3263f46b3f79f49cdb0c34
Elias Carlston <elias@eliasjames.com>
2016-10-13 16:32:18 -0500

    Backbone router example

---
+++ examples/src/appRouter.js
@@ -0,0 +1,19 @@
+var AppRouter = Backbone.Router.extend({
+  routes: {
+    '': 'home',
+    'about': 'about',
+    'songs': 'songs'
+  },
+  about: function about() {
+    $('body').html('<h2>about</h2>');
+  },
+  home: function home() {
+    $('body').html('<h2>home</h2>');
+  },
+  songs: function songs() {
+    $('body').html( app.songsList.$el );
+  }
+});
+var appRouter = new AppRouter();
+
+Backbone.history.start();
